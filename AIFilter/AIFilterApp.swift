634dd0250a0a8c5b85c910940224bd40e24dfb4e
AyanKhan98 <ayankhann98@gmail.com>
2026-03-04 12:54:16 +0530

    Initial Commit

---
+++ AIFilter/AIFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIFilterApp.swift
+//  AIFilter
+//
+//  Created by Ayan khan on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AIFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
